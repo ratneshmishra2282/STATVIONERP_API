d155c3909ad41ea206cdcc5966604800b466a69b
Ratnesh Mishra <63775997+ratneshmishra2282@users.noreply.github.com>
2026-03-09 10:14:26 +0530

    Delete ERP/ERP directory

--- ERP/ERP/config.py
+++
@@ -1,7 +0,0 @@
-
-class Config:
-    SECRET_KEY = "supersecret"
-    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:password@localhost/erp"
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    JWT_SECRET_KEY = "jwt-secret"
-    MONGO_URI = "mongodb://localhost:27017/erp"
