14fb9da4ea66a2a9dad5e4a488736b928746247d
Ratnesh Mishra <63775997+ratneshmishra2282@users.noreply.github.com>
2026-03-17 22:00:39 +0530

    Delete ERP 2/ERP directory

--- ERP 2/ERP/extensions.py
+++
@@ -1,9 +0,0 @@
-from flask_sqlalchemy import SQLAlchemy
-from flask_jwt_extended import JWTManager
-from flask_pymongo import PyMongo
-from flask_restful import Api
-
-db = SQLAlchemy()
-jwt = JWTManager()
-mongo = PyMongo()
-api = Api()
